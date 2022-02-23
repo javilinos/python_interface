bca86d81f59fb6f081907fb35f83838ccad02ee3
Pedro Arias <pedroariasperez96@gmail.com>
2022-02-23 17:48:47 +0100

    Added orientation to odom lock

--- python_interface/drone_interface.py
+++ python_interface/drone_interface.py
@@ -10,10 +10,37 @@ from time import sleep
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import NavSatFix
 from rclpy.timer import Rate
+from geometry_msgs.msg import Quaternion
+
+import math
+
+
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+     
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+     
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+     
+    return roll_x, pitch_y, yaw_z # in radians
 
 
 class DroneInterface(Node):
     pose = [0,0,0]
+    orientation = [0,0,0]
     odom_lock = threading.Lock()
     fix = [0,0,0]
     gps_lock = threading.Lock()
@@ -42,11 +69,17 @@ class DroneInterface(Node):
     @odom_lock_decor
     def odometry_callback(self, msg):
         self.pose = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z]
+        self.orientation = [*euler_from_quaternion(msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, 
+                                                   msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)]
         
     @odom_lock_decor
     def get_position(self):
         # print(self.pose)
         return self.pose.copy()
+
+    @odom_lock_decor
+    def get_orientation(self):
+        return self.orientation.copy()
 
     def gps_lock_decor(func):
         def wrapper(self, *args, **kwargs):
