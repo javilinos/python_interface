8ea0eb2145d9c80f99e2381119345c1b60e69c8d
Pedro Arias <pedroariasperez96@gmail.com>
2022-03-09 13:41:29 +0100

    FollowPath action improved and adapted to gps translator services

--- python_interface/drone_interface.py
+++ python_interface/drone_interface.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 from matplotlib.pyplot import rc
 import rclpy, threading
 from rclpy import publisher, spin_once
@@ -9,18 +10,19 @@ from rclpy.action import ActionClient
 from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy
 from rclpy.qos import QoSProfile
 from time import sleep
+from dataclasses import dataclass
 
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, Path
 from sensor_msgs.msg import NavSatFix
 from rclpy.timer import Rate
-from geometry_msgs.msg import Quaternion
 from as2_msgs.action import FollowPath, GoToWaypoint
 from as2_msgs.msg import TrajectoryWaypoints, PlatformInfo
 from geometry_msgs.msg import PoseStamped, Pose
 from action_msgs.msg import GoalStatus
-from as2_msgs.srv import SetOrigin
+from as2_msgs.srv import SetOrigin, GeopathToPath, PathToGeopath
+from geographic_msgs.msg import GeoPath, GeoPoseStamped
 
-import math
+from utils import euler_from_quaternion, path_to_list
 
 
 STATE = ["DISARMED", "LANDED", "TAKING_OFF", "FLYING", "LANDING", "EMERGENCY"]
@@ -28,28 +30,6 @@ YAW_MODE = ["YAW_ANGLE", "YAW_SPEED"]
 CONTROL_MODE = ["POSITION_MODE", "SPEED_MODE", "SPEED_IN_A_PLANE", "ACCEL_MODE", "ATTITUDE_MODE", "ACRO_MODE", "UNSET"]
 REFERENCE_FRAME = ["LOCAL_ENU_FRAME", "BODY_FLU_FRAME", "GLOBAL_ENU_FRAME"]
 
-
-def euler_from_quaternion(x, y, z, w):
-    """
-    Convert a quaternion into euler angles (roll, pitch, yaw)
-    roll is rotation around x in radians (counterclockwise)
-    pitch is rotation around y in radians (counterclockwise)
-    yaw is rotation around z in radians (counterclockwise)
-    """
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    roll_x = math.atan2(t0, t1)
-     
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch_y = math.asin(t2)
-     
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (y * y + z * z)
-    yaw_z = math.atan2(t3, t4)
-     
-    return roll_x, pitch_y, yaw_z # in radians
 
 class ActionHandler:
     TIMEOUT = 3  # seconds
@@ -102,11 +82,19 @@ class ActionHandler:
 
 
 class SendFollowPath(ActionHandler):
-    def __init__(self, drone, path):
+    @dataclass
+    class FollowPathData:
+        path: Any
+        speed: float = 1.0
+        yaw_mode: TrajectoryWaypoints.yaw_mode = TrajectoryWaypoints.KEEP_YAW
+        is_gps: bool = False
+        
+    def __init__(self, drone, path_data):
         self._action_client = ActionClient(drone, FollowPath, f'{drone.get_drone_id()}/FollowPathBehaviour')
+        self._drone = drone
 
         goal_msg = FollowPath.Goal()
-        goal_msg.trajectory_waypoints = path
+        goal_msg.trajectory_waypoints = self.get_traj(path_data)
 
         try:
             super().__init__(self._action_client, goal_msg, drone.get_logger())
@@ -114,6 +102,66 @@ class SendFollowPath(ActionHandler):
             drone.get_logger().error(str(err))
         except (self.GoalRejected, self.GoalFailed) as err:
             drone.get_logger().warn(str(err))
+
+    def get_traj(self, path_data):
+        if isinstance(path_data.path, list):
+            if not path_data.path:  # not empty
+                raise Exception  # TODO
+            if isinstance(path_data.path[0], list):
+                point_list = path_data.path
+            else:
+                point_list = [path_data.path]
+        elif isinstance(path_data.path, tuple):
+            point_list = [list(path_data.path)]
+        elif isinstance(path_data.path, Path):
+            point_list = path_to_list(path_data.path)
+            is_gps = False
+        elif isinstance(path_data.path, GeoPath):
+            req = GeopathToPath.Request()
+            req.geo_path = path_data.path
+            resp = self._drone.global_to_local_cli_.call(req)
+            if not resp.success:
+                self._drone.get_logger().warn("Can't follow path since origin is not set")
+                raise Exception  # TODO
+
+            point_list = path_to_list(resp.path)
+            is_gps = False
+        else:
+            raise Exception  # TODO
+        
+        if path_data.is_gps:
+            geopath = GeoPath()
+            geopath.header.stamp = self._drone.get_clock().now().to_msg()
+            geopath.header.frame_id = "wgs84"
+            for wp in point_list:
+                gps = GeoPoseStamped()
+                gps.header.stamp = geopath.header.stamp
+                gps.header.frame_id = geopath.header.frame_id
+                gps.pose.position.latitude = float(wp[0])
+                gps.pose.position.longitude = float(wp[1])
+                gps.pose.position.altitude = float(wp[2])
+                geopath.poses.append(gps)
+        
+            path_data.path = geopath
+            path_data.is_gps = False
+            return self.get_traj(path_data)
+        else:
+            msg = TrajectoryWaypoints()
+            msg.header.stamp = self._drone.get_clock().now().to_msg()
+            msg.header.frame_id = "odom"
+            msg.yaw_mode = path_data.yaw_mode
+            poses = []
+            for point in point_list:
+                pose = PoseStamped()
+                x,y,z = point
+                pose.pose.position.x = (float)(x)
+                pose.pose.position.y = (float)(y)
+                pose.pose.position.z = (float)(z)
+                pose.pose.orientation.w=1.0
+                poses.append(pose)
+            msg.poses = poses
+            msg.max_speed = (float)(path_data.speed)
+            return msg
 
 
 class SendGoToWaypoint(ActionHandler):
@@ -130,7 +178,6 @@ class SendGoToWaypoint(ActionHandler):
             drone.get_logger().error(str(err))
         except (self.GoalRejected, self.GoalFailed) as err:
             drone.get_logger().warn(str(err))
-
 
 
 class DroneInterface(Node):
@@ -153,12 +200,9 @@ class DroneInterface(Node):
         self.odom_sub = self.create_subscription(Odometry, f'{self.get_drone_id()}/self_localization/odom', self.odometry_callback, QoSProfile(depth=10))
         self.gps_sub = self.create_subscription(NavSatFix, f'{self.get_drone_id()}/platform/gps', self.gps_callback, QoSProfile(depth=10))
 
-        self.globals = []
-        self.locals = []
         translator_namespace = ""
-        self.fix_pub_ = self.create_publisher(NavSatFix, f"{translator_namespace}/global_pose/fix", 10)
-        self.global_sub_ = self.create_subscription(PoseStamped, f"{translator_namespace}/global_pose/ecef", self.global_callback, 10)
-        self.local_sub_ = self.create_subscription(PoseStamped, f"{translator_namespace}/local_pose", self.local_callback, 10)
+        self.global_to_local_cli_ = self.create_client(GeopathToPath, f"{translator_namespace}/geopath_to_path")
+        self.local_to_global_cli_ = self.create_client(PathToGeopath, f"{translator_namespace}/path_to_geopath")
 
         self.set_origin_cli_ = self.create_client(SetOrigin, f"{translator_namespace}/set_origin")
         if not self.set_origin_cli_.wait_for_service(timeout_sec=10):
@@ -238,31 +282,16 @@ class DroneInterface(Node):
         gps_pose = self.get_gps_pose()
 
         req = SetOrigin.Request()
-        req.origin.latitude = gps_pose[0]
-        req.origin.longitude = gps_pose[1]
-        req.origin.altitude = gps_pose[2]
+        req.origin.latitude = float(gps_pose[0])
+        req.origin.longitude = float(gps_pose[1])
+        req.origin.altitude = float(gps_pose[2])
         resp = self.set_origin_cli_.call(req)
         if not resp.success:
             self.get_logger().warn("Origin already set")
 
-    def __follow_path(self, point_list, speed, yaw_mode = TrajectoryWaypoints.KEEP_YAW):
-        msg = TrajectoryWaypoints()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = "odom"
-        msg.yaw_mode = yaw_mode
-        poses = []
-        for point in point_list:
-            pose = PoseStamped()
-            x,y,z = point
-            pose.pose.position.x = (float)(x)
-            pose.pose.position.y = (float)(y)
-            pose.pose.position.z = (float)(z)
-            pose.pose.orientation.w=1.0
-            poses.append(pose)
-        msg.poses = poses
-        msg.max_speed = (float)(speed)
-
-        SendFollowPath(self, msg)
+    def __follow_path(self, path, speed=1.0, yaw_mode = TrajectoryWaypoints.KEEP_YAW, is_gps=False):
+        path_data = SendFollowPath.FollowPathData(path, speed, yaw_mode, is_gps)
+        SendFollowPath(self, path_data)
 
     def takeoff(self, height=1.0, speed=0.5):
         self.__set_home()
@@ -274,47 +303,13 @@ class DroneInterface(Node):
         self.__follow_path(path, speed, TrajectoryWaypoints.PATH_FACING)
 
     def follow_gps_path(self, wp_path, speed=1.0):
-        for wp in wp_path:
-            msg = NavSatFix()
-            msg.header.stamp = self.get_clock().now().to_msg()
-            msg.header.frame_id = "wgs84"
-            msg.latitude = float(wp[0])
-            msg.longitude = float(wp[1])
-            msg.altitude = float(wp[2])
-            self.fix_pub_.publish(msg)
-        
-        sleep(1)
-        while len(wp_path) != len(self.locals):
-            sleep(0.5)
-            # return
-
-        print(self.locals)
-        self.__follow_path(self.locals, speed, TrajectoryWaypoints.PATH_FACING)
-        self.locals = []
-        self.globals = []
+        self.__follow_path(wp_path, speed, TrajectoryWaypoints.PATH_FACING, is_gps=True)
 
     # TEMPORAL
     def follow_gps_wp(self, wp_list, speed=1.0):
-        for wp in wp_list:
-            msg = NavSatFix()
-            msg.header.stamp = self.get_clock().now().to_msg()
-            msg.header.frame_id = "wgs84"
-            msg.latitude = float(wp[0])
-            msg.longitude = float(wp[1])
-            msg.altitude = float(wp[2])
-            self.fix_pub_.publish(msg)
+        for pnt in wp_list:
+            self.__follow_path([pnt], speed, TrajectoryWaypoints.PATH_FACING, is_gps=True)
         
-        sleep(1)
-        while len(wp_list) != len(self.locals):
-            sleep(0.5)
-            # return
-
-        for pnt in self.locals:
-            self.__follow_path([pnt], speed, TrajectoryWaypoints.PATH_FACING)
-        
-        self.locals = []
-        self.globals = []
-
     def land(self):
         pose  = self.get_position()[:2]
         self.__follow_path([pose + [-0.5]], 0.3, TrajectoryWaypoints.KEEP_YAW)
---
+++ python_interface/utils.py
@@ -0,0 +1,29 @@
+
+from math import atan2, asin
+
+
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = atan2(t0, t1)
+     
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = asin(t2)
+     
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = atan2(t3, t4)
+     
+    return roll_x, pitch_y, yaw_z # in radians
+
+
+def path_to_list(path):
+    return list(map(lambda p: [p.pose.position.x, p.pose.position.y, p.pose.position.z], path.poses))
