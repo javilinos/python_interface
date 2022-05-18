ecd5789027ed899156c6edbc0b53438716cd3289
Pedro Arias <pedroariasperez96@gmail.com>
2022-05-18 18:42:47 +0200

    Added copyright

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'python_interface'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='miguel',
-    maintainer_email='miferco97@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Computer Vision And Aerial Robotics Group (UPM)',
+    maintainer_email='cvar.upm3@gmail.com',
+    description='Python interface tool',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
